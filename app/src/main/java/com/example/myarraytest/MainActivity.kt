3856779e6a0d8ae3c562e568e2cb5e8a6cd85c04
Ypypy <kkkmit@mail.ru>
2022-01-17 15:17:57 +0300

    reverse array

--- app/src/main/java/com/example/myarraytest/MainActivity.kt
+++ app/src/main/java/com/example/myarraytest/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.myarraytest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var array: Array<Int> = arrayOf(3,4,5,6)
+        val numbers = arrayOf(3,2,5,6)
+        Log.v("MyLog", "Number ${numbers.contentToString()}")
+        numbers.reverse()
+        Log.v("MyLog", "Number ${numbers.contentToString()}")
     }
 }
