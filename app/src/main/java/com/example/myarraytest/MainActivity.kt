ff0e39dcd610e230a89373a5e426e83b889fef46
Ypypy <kkkmit@mail.ru>
2022-01-17 21:29:27 +0300

    sort array

--- app/src/main/java/com/example/myarraytest/MainActivity.kt
+++ app/src/main/java/com/example/myarraytest/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val numbers = arrayOf(3,2,5,6)
         Log.v("MyLog", "Number ${numbers.contentToString()}")
-        numbers.reverse()
+        numbers.sort()
         Log.v("MyLog", "Number ${numbers.contentToString()}")
     }
 }
