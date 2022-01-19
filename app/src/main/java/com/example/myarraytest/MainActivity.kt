3d0ed55770d823b2060f6a471bf7de54c2f3175b
Ypypy <kkkmit@mail.ru>
2022-01-19 21:20:28 +0300

    distribution of pupils

--- app/src/main/java/com/example/myarraytest/MainActivity.kt
+++ app/src/main/java/com/example/myarraytest/MainActivity.kt
@@ -5,20 +5,36 @@ import android.os.Bundle
 import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    // сортировка учеников по успеваемости
 
-    val lostArray = arrayOf(10000, 2300, 45000, 65000,6500, 400) // выложил
-    val earnArray = arrayOf(15000, 300, 345000, 5000,16500, 3400) // заработал
-    val resultArray = arrayListOf<String>()
+    val bad = 0..3
+    val normal = 4..6
+    val nice = 7..9
+    val excellent = 10
+    val gradeArray = arrayOf(4, 7, 3, 6, 10, 2)
+    val nameArray = arrayOf("Антон", "Егор", "Маша", "Светлана", "Юля", "Семен")
+    val badArray = ArrayList<String>()
+    val normalArray = ArrayList<String>()
+    val niceArray = ArrayList<String>()
+    val excellentArray = ArrayList<String>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val names = resources.getStringArray(R.array.names) //достаем из ресурсов массив
 
-        for((index, name) in names.withIndex() ){
-            resultArray.add("Name: $name - polychil = ${earnArray[index]-lostArray[index]}")
-            Log.v("MyLog", "${resultArray[index]}")
-        }
+         for ((index, grade) in gradeArray.withIndex()){
+             when (grade){
+                 in bad -> {badArray.add("Ученик: ${nameArray[index]} – $grade")}
+                 in normal -> {normalArray.add("Ученик: ${nameArray[index]} – $grade")}
+                 in nice -> {niceArray.add("Ученик: ${nameArray[index]} – $grade")}
+                 excellent -> {excellentArray.add("Ученик: ${nameArray[index]} – $grade")}
+                }
 
+            }
+        badArray.forEach{Log.d("MyLog", it)}
+        normalArray.forEach{Log.d("MyLog", it)}
+        niceArray.forEach{Log.d("MyLog", it)}
+        excellentArray.forEach{Log.d("MyLog", it)}
         }
     }
